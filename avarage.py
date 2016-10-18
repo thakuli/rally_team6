7e86f18cd99f4c277af986484176f2ea707bf78a
mikheikk <mika.heikkinen1@gmail.com>
2016-10-18 15:21:00 +0300

    avarage class

---
+++ avarage.py
@@ -0,0 +1,17 @@
+class Avarage():
+    def __init__(self):
+        self.values = []
+
+    def add(self, value):
+        self.values.append()
+        if self.values.append() is 5:
+            self.values.pop()
+
+    def get_avararage(self):
+
+        if self.values.count() is 4:
+            sum = sum(self.values)
+            return sum / len(self.values)
+
+        else:
+            return 1
