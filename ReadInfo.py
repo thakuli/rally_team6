d41a72b4eb2c6cc14ec0dac045da3504e286c5ca
thakuli <tero.hakuli@nokia.com>
2016-10-18 09:52:53 +0300

    dao dao

--- ReadInfo.py
+++ ReadInfo.py
@@ -1,22 +1,14 @@
 
 import json
+from dao.Data import Beacon
+from dao.Data import Coordinate
+from dao.Data import Compass
 
-class Compass:
-    def __init__(self, x, y , z):
-        self.x = x
-        self.y = y
-        self.z = z
-        
-    def to_string(self):
-        "x={}, y={}, z={}".format(self.x, self.y, self.z)
 
 class ReadInfo:
-    def __init__(self):
-        self.baddr = ""
-        self.rssi = ""
-        self.time_to_go = 0
-        self.command_id = 255
-        self.compass = None
+
+    def __init__(self, json):
+        self.set_data(json)
 
     def set_data(self, json_data):
     	self.baddr = json_data['baddr']
@@ -25,16 +17,24 @@ class ReadInfo:
         self.command_id = json_data['command_id']
         self.compass = Compass(json_data['x'], json_data['y'], json_data['y']) 
 
+    def to_string(self):
+        print "baddr={}, rssi={}, time_to_go={}, command_id={}, compass={}".format(self.baddr, self.rssi, self.time_to_go, self.command_id, self.compass.to_string())
 
+
+def read_json(json_data):
+    return json.loads(json_data)
+
+
+# testausta
 def testi():
     test_data='{"baddr" : "71:E9:F6:72:21:FB", "rssi" : "-72", "time_to_go" : "0", "command_id" : "255", "x" : "-90", "y" : "147", "z" : "-127"}'
     	
-    jsonD = json.loads(test_data)
+    ri = ReadInfo(read_json(test_data))
+    ri.to_string()
 
-    ri = ReadInfo()
-    ri.set_data(jsonD)
-
-    print "baddr={}, rssi={}, time_to_go={}, command_id={}, compass={}".format(ri.baddr, ri.rssi, ri.time_to_go, ri.command_id, ri.compass.to_string())
+    
 
 # main
-testi()
+if __name__ == "__main__":
+    testi()
+
--- dao/Data.py
+++ dao/Data.py
@@ -1,3 +1,5 @@
+import math
+
 class Beacon:
     def __init__(self, baddr, rssi):
         self.baddr = baddr
@@ -18,3 +20,25 @@ class Compass:
         
     def to_string(self):
         return "x={}, y={}, z={}".format(self.x, self.y, self.z)
+
+    def get_angle(self):
+        tmp=float(float(self.y) / float(self.x))
+        arcci=math.degrees(math.asin(tmp))
+
+        return arcci
+
+def testi():
+    cp = Compass(220, 203, 82)
+    print cp.to_string()
+    print "angle={}".format(cp.get_angle())
+    
+    cp = Compass(148, 137, 59)
+    print cp.to_string()
+    print "angle={}".format(cp.get_angle())
+
+
+# main
+if __name__ == "__main__":
+    testi()
+
+
